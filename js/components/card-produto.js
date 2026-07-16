95ecd6db42fc0a587d4d2f5bcf811c92676f4d4c
Silvia Abe <silviaabebuzatto@gmail.com>
2026-07-16 14:59:49 -0300

    feat: seção Destaques (com cards)

---
+++ js/components/card-produto.js
@@ -0,0 +1,20 @@
+class Card extends HTMLElement {
+  connectedCallback() {
+    const src = this.getAttribute("src") || "";
+    const categoria = this.getAttribute('categoria') || '';
+    const titulo = this.getAttribute('titulo') || '';
+    const preco = this.getAttribute('preco') || '';
+
+    this.innerHTML = `
+    <div class="card">
+      <img src="${src}" alt="${titulo}">
+      <span class="categoria">${categoria}</span>
+      <h3 class="titulo">${titulo}</h3>
+      <p class="preco">${preco}</p>
+      <button>Ver detalhes</button>
+    </div>
+    `
+  }
+}
+
+customElements.define('card-produto', Card)
