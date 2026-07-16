337efb3f183922d39b135ed78ea58c7d74713347
Silvia Abe <silviaabebuzatto@gmail.com>
2026-07-16 16:58:48 -0300

    feat: footer do projeto

---
+++ js/components/site-footer.js
@@ -0,0 +1,35 @@
+class SiteFooter extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+    <footer class="footer">
+      <h3>Casa Conceito Decor</h3>
+      <p>Transformando ambientes com elegância e personalidade.</p>
+      <div class="footer__redes">
+        <a href="#" aria-label="Instagram">
+          <i class="fa-brands fa-instagram"></i>
+        </a>
+        <a href="#" aria-label="Facebook">
+          <i class="fa-brands fa-facebook-f"></i>
+        </a>
+        <a href="#" aria-label="TikTok">
+          <i class="fa-brands fa-tiktok"></i>
+        </a>
+        <a href="#" aria-label="Twitter">
+          <i class="fa-brands fa-x-twitter"></i>
+        </a>
+        <a href="#" aria-label="Pinterest">
+          <i class="fa-brands fa-pinterest-p"></i>
+        </a>
+        <a href="#" aria-label="YouTube">
+          <i class="fa-brands fa-youtube"></i>
+        </a>
+      </div>
+      <p class="footer__copy">
+        © 2026 Casa Conceito Decor. Todos os direitos reservados.
+      </p>
+    </footer>
+    `
+  }
+}
+
+customElements.define('site-footer', SiteFooter)
